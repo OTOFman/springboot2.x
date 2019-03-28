de17410629d2311883357f8cbb18bc53ad729982
OTOFman <zryu@1314-edu.com>
2019-03-28 09:16:34 +0800

    add sample of http get

---
+++ src/main/java/com/otof/springboot2/x/Controller/GetController.java
@@ -0,0 +1,27 @@
+package com.otof.springboot2.x.Controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("get_method")
+public class GetController {
+
+    @GetMapping(path = "mode_1/{name}/{age}")
+    public Map getUrlPathParam(@PathVariable("name") String name, @PathVariable String age) {
+        return new HashMap(){{
+            put("name", name);
+            put("age", age);
+        }};
+    }
+
+    @GetMapping("mode_2")
+    public Map getQueryParam(@RequestParam(value = "name") String name, @RequestParam(defaultValue = "100") String age) {
+        return new HashMap(){{
+            put("name", name);
+            put("age", age);
+        }};
+    }
+}
